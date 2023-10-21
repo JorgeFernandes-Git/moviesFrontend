197ac9b6c74721e4536212cceb60d52c1013ef42
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-10-21 15:05:42 +0100

    Add post reviews on movies.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import './App.css';
-import api from "./api/axiosConfig";
+import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
+  const [uniqueKey, setUniqueKey] = useState(null);
+
   const getMovies = async () => {
     try {
       const response = await api.get("/api/v1/movies");
-      console.log(response.data)
-      setMovies(response.data)
+      setMovies(response.data);
     } catch (err) {
-      console.log(err)
+      console.log(err);
+    }
+  }
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviewsIds);
+    } catch (error) {
+      console.error(error);
     }
   }
 
@@ -22,15 +39,25 @@ function App() {
     getMovies();
   }, [])
 
+  useEffect(() => {
+    setUniqueKey(uuidv4());
+  }, []);
+
+  const reviewsComponent = uniqueKey ? (
+    <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} uniqueKey={uniqueKey} />} />
+  ) : null;
+
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Layout />} >
-          <Route path='/' element={<Home movies={movies} />} ></Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />} />
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />} />
+          {reviewsComponent}
         </Route>
       </Routes>
-    </div >
+    </div>
   );
 }
 
